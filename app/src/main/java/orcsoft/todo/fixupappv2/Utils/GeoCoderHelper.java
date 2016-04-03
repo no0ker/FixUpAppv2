b95e1763f81363e624a43c08f198eb9d7835d99a
rush0714 <pukanGi9>
2016-04-04 01:07:36 +0300

    comit

---
+++ app/src/main/java/orcsoft/todo/fixupappv2/Utils/GeoCoderHelper.java
@@ -0,0 +1,85 @@
+package orcsoft.todo.fixupappv2.Utils;
+
+import android.util.Log;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import orcsoft.todo.fixupappv2.Entity.GeoPoint;
+
+public class GeoCoderHelper {
+    private final String baseAddress = "https://geocode-maps.yandex.ru/1.x/?geocode=";
+    public final String TAG = GeoCoderHelper.class.toString();
+
+    public GeoPoint getGeoPoint(String address) {
+        GeoPoint result = new GeoPoint();
+        try {
+            address = address.replaceAll("\\s", "+");
+            URL url = new URL(baseAddress + URLEncoder.encode(address, "UTF-8"));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            if (200 != connection.getResponseCode()) {
+                throw new Exception("ошибка при получении координат. код ошибки" + connection.getResponseCode());
+            }
+
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            mySaxparser saxp = new mySaxparser();
+            parser.parse(connection.getInputStream(), saxp);
+            String parsedRsult = saxp.getValue();
+            Pattern pattern = Pattern.compile("(\\d+.\\d+)\\s(\\d+.\\d+)");
+            Matcher matchsr = pattern.matcher(parsedRsult);
+            if (matchsr.matches()) {
+                result.setLongitude(Double.parseDouble(matchsr.group(1)));
+                result.setLatitude(Double.parseDouble(matchsr.group(2)));
+                Log.d(TAG, result.getLongitude() + "  " + result.getLatitude() + "  " + address);
+            }
+        } catch (Exception e) {
+            Log.d(TAG, Log.getStackTraceString(e));
+        }
+        return result;
+    }
+
+    private class mySaxparser extends DefaultHandler {
+        private boolean found = false;
+        private boolean thisElement;
+        private String value;
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            if (qName.equals("pos") && !found) {
+                thisElement = true;
+            } else {
+                thisElement = false;
+            }
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            thisElement = false;
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            if (thisElement) {
+                value = new String(ch, start, length);
+                found = true;
+            }
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+}
